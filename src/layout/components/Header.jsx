6ba3a83ef5ae1f7d9b5dd2b09424803b93d7f21e
rtaeho <ryou0920@naver.com>
2024-07-15 21:48:40 +0900

    feat : 프로젝트 구조 수정

--- src/layout/components/Header.jsx
+++ src/layout/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import { ReactComponent as Soccer } from '../../assets/Soccer.svg';
+
 const Header = () => {
   return (
     <div className="head-container ">
-      <div>S-Cul</div>
+      <div>S-Cul </div> <Soccer />
     </div>
   );
 };
