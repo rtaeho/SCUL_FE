61712df9970dd1ff459ed4c95ef733821feb3217
SAEPARAN <harami8512@gmail.com>
2024-07-21 00:55:12 +0900

    initial_1

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Layout from '../layout';
 const Main = lazy(() => import('../page/main'));
 const Auth = lazy(() => import('../page/auth'));
 const Community = lazy(() => import('../page/community'));
+const Initial = lazy(() => import('../page/initial'));
 const Club = lazy(() => import('../page/club'));
 const Policy = lazy(() => import('../page/policy'));
 const Loading = () => <>로딩중입니다.</>;
@@ -34,6 +35,14 @@ const router = createBrowserRouter([
         element: (
           <Suspense fallback={<Loading />}>
             <Community />
+          </Suspense>
+        ),
+      },
+      {
+        path: 'initial',
+        element: (
+          <Suspense fallback={<Loading />}>
+            <Initial />
           </Suspense>
         ),
       },
