accfa79cf1c1049990fab4e24b30fdec8e2bdf31
rtaeho <ryou0920@naver.com>
2024-07-25 02:18:28 +0900

    feat : 필터 컴포넌트 구현
    필터 컴포넌트 추가 정렬 필요

--- src/page/community/index.jsx
+++ src/page/community/index.jsx
@@ -1,5 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { ReactComponent as Select } from '../../assets/images/FilterSelect.svg';
+import { ReactComponent as Search } from '../../assets/images/FilterSearch.svg';
+
+const tags = {
+  free: ['전체', '소모임', '용품', '운동', '시설'],
+  review: ['전체', '리뷰', '추천', '경험담'],
+  info: ['전체', '공지', '이벤트', '문의'],
+};
 
 // 게시판 목록 컴포넌트
 const BoardList = ({ boardName }) => {
@@ -15,43 +23,154 @@ const WriteButton = ({ onWrite }) => {
   );
 };
 
-// 공지사항 필터 컴포넌트
-const Filter = ({ onFilterChange }) => {
-  const [selectedCategory, setSelectedCategory] = useState('all');
+// 필터 컴포넌트
+const Filter = ({ tags, onFilterChange, onTagChange, onSearch }) => {
+  const [selectedSort, setSelectedSort] = useState('최신 순');
+  const [selectedTag, setSelectedTag] = useState('전체');
+  const [selectedSearchOption, setSelectedSearchOption] = useState('제목');
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [sortDropdownVisible, setSortDropdownVisible] = useState(false);
+  const [searchDropdownVisible, setSearchDropdownVisible] = useState(false);
 
-  const handleCategoryChange = (event) => {
-    const category = event.target.value;
-    setSelectedCategory(category);
-    onFilterChange(category);
+  const handleSortChange = (sort) => {
+    setSelectedSort(sort);
+    setSortDropdownVisible(false);
+    onFilterChange({
+      sort,
+      tag: selectedTag,
+      searchOption: selectedSearchOption,
+      keyword: searchKeyword,
+    });
+  };
+
+  const handleTagChange = (tag) => {
+    setSelectedTag(tag);
+    onTagChange({
+      sort: selectedSort,
+      tag,
+      searchOption: selectedSearchOption,
+      keyword: searchKeyword,
+    });
+  };
+
+  const handleSearchOptionChange = (searchOption) => {
+    setSelectedSearchOption(searchOption);
+    setSearchDropdownVisible(false);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchKeyword(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    if (event.key === 'Enter') {
+      onSearch({
+        sort: selectedSort,
+        tag: selectedTag,
+        searchOption: selectedSearchOption,
+        keyword: searchKeyword,
+      });
+    }
   };
 
   return (
-    <div className="filter">
-      <label htmlFor="category">카테고리 필터: </label>
-      <select
-        id="category"
-        value={selectedCategory}
-        onChange={handleCategoryChange}
-      >
-        <option value="all">모두</option>
-        <option value="general">일반</option>
-        <option value="important">중요</option>
-        <option value="updates">업데이트</option>
-      </select>
-    </div>
-  );
-};
-
-// 공지사항 컴포넌트
-const Notice = ({ notices }) => {
-  return (
-    <div className="notice">
-      <h3>공지사항</h3>
-      <ul>
-        {notices.map((notice, index) => (
-          <li key={index}>{notice}</li>
-        ))}
-      </ul>
+    <div className="board-filter-container">
+      <div className="board-filter-select-container">
+        <div className="board-filter-selected">{selectedSort}</div>
+        <Select
+          className="board-filter-select-button"
+          onClick={() => setSortDropdownVisible(!sortDropdownVisible)}
+        />
+        {sortDropdownVisible && (
+          <ul className="board-filter-select-dropdown-container">
+            <li
+              key="최신 순"
+              className="board-filter-select-dropdown-list"
+              onClick={() => handleSortChange('최신 순')}
+            >
+              최신 순
+            </li>
+            <hr className="board-filter-select-dropdown-list-hr" />
+            <li
+              key="조회 순"
+              className="board-filter-select-dropdown-list"
+              onClick={() => handleSortChange('조회 순')}
+            >
+              조회 순
+            </li>
+            <hr className="board-filter-select-dropdown-list-hr" />
+            <li
+              key="댓글 순"
+              className="board-filter-select-dropdown-list"
+              onClick={() => handleSortChange('댓글 순')}
+            >
+              댓글 순
+            </li>
+          </ul>
+        )}
+      </div>
+      <div className="board-filter-tag-container">
+        <ul className="board-filter-tag-list-container">
+          {tags.map((tag) => (
+            <li
+              key={tag}
+              onClick={() => handleTagChange(tag)}
+              className={
+                selectedTag === tag
+                  ? 'board-filter-tag-list-selected'
+                  : 'board-filter-tag-list'
+              }
+            >
+              {tag}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="board-filter-search-container">
+        <div className="board-filter-search-select-container">
+          <div
+            className="board-filter-search-select-selected"
+            onClick={() => setSearchDropdownVisible(!searchDropdownVisible)}
+          >
+            {selectedSearchOption}
+            {searchDropdownVisible && (
+              <ul className="board-filter-search-select-dropdown-container">
+                <li
+                  className="board-filter-search-select-dropdown-list"
+                  onClick={() => handleSearchOptionChange('제목')}
+                >
+                  제목
+                </li>
+                <hr className="board-filter-search-select-dropdown-list-hr" />
+                <li
+                  className="board-filter-search-select-dropdown-list"
+                  onClick={() => handleSearchOptionChange('작성자')}
+                >
+                  작성자
+                </li>
+                <hr className="board-filter-search-select-dropdown-list-hr" />
+                <li
+                  className="board-filter-search-select-dropdown-list"
+                  onClick={() => handleSearchOptionChange('내용')}
+                >
+                  내용
+                </li>
+              </ul>
+            )}
+          </div>
+        </div>
+        <div className="board-filter-search-input-container">
+          <Search className="board-filter-search-input-icon" />
+          <input
+            className="board-filter-search-input"
+            type="text"
+            value={searchKeyword}
+            onChange={handleSearchChange}
+            onKeyDown={handleSearchSubmit}
+            placeholder="검색어를 입력해 주세요"
+          />
+        </div>
+      </div>
     </div>
   );
 };
@@ -95,11 +214,33 @@ const Community = () => {
   const { board } = useParams();
   const { sport } = useParams();
   const [currentPage, setCurrentPage] = useState(1);
-  const [filteredNotices, setFilteredNotices] = useState([
-    '공지사항 1',
-    '공지사항 2',
-  ]);
+  const [posts, setPosts] = useState([]);
   const totalPages = 5;
+
+  // 게시판별 태그 배열 정의
+  const tags = {
+    free: ['전체', '소모임', '용품', '운동', '시설'],
+    review: ['전체', '리뷰', '추천', '경험담'],
+    info: ['전체', '공지', '이벤트', '문의'],
+  };
+
+  // 현재 게시판에 맞는 태그 배열 선택
+  const currentTags = tags[board] || [];
+
+  useEffect(() => {
+    //fetchPosts();
+  }, [currentPage]);
+
+  const fetchPosts = async (filterParams) => {
+    // API 호출 로직 추가
+    // const response = await fetch('/api/posts', {
+    //   method: 'POST',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify({ page: currentPage, ...filterParams }),
+    // });
+    // const data = await response.json();
+    // setPosts(data.posts);
+  };
 
   const handleWriteClick = () => {
     alert('글 작성 버튼 클릭됨');
@@ -111,17 +252,16 @@ const Community = () => {
     }
   };
 
-  const handleFilterChange = (category) => {
-    // 필터 로직을 추가하여 공지사항을 필터링합니다.
-    const allNotices = ['공지사항 1', '공지사항 2', '공지사항 3', '공지사항 4']; // 모든 공지사항
-    if (category === 'all') {
-      setFilteredNotices(allNotices);
-    } else {
-      // 예시: 필터링 로직 추가 (단순히 카테고리 이름에 따라 공지사항을 필터링합니다)
-      setFilteredNotices(
-        allNotices.filter((notice) => notice.includes(category))
-      );
-    }
+  const handleFilterChange = (filterParams) => {
+    fetchPosts(filterParams);
+  };
+
+  const handleTagChange = (filterParams) => {
+    fetchPosts(filterParams);
+  };
+
+  const handleSearch = (searchParams) => {
+    fetchPosts(searchParams);
   };
 
   const boardName =
@@ -131,20 +271,18 @@ const Community = () => {
       ? '후기 게시판'
       : '정보 게시판';
 
-  const posts = [
-    { title: '게시글 1' },
-    { title: '게시글 2' },
-    { title: '게시글 3' },
-  ];
-
   return (
     <div className="board-container">
       <div className="board-header">
         <BoardList boardName={boardName} />
         <WriteButton onWrite={handleWriteClick} />
       </div>
-      <Filter onFilterChange={handleFilterChange} />
-      <Notice notices={filteredNotices} />
+      <Filter
+        tags={currentTags} // 현재 게시판에 맞는 태그 배열 전달
+        onFilterChange={handleFilterChange}
+        onTagChange={handleTagChange}
+        onSearch={handleSearch}
+      />
       <PostList posts={posts} />
       <Pagination
         currentPage={currentPage}
@@ -154,5 +292,4 @@ const Community = () => {
     </div>
   );
 };
-
 export default Community;
